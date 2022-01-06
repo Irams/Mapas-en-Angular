021f2eb030e332f9d710c8c9be203f932a0a4d61
Irams <herber.medina@gmail.com>
2022-01-05 21:58:40 -0600

    Clase 316 terminada, zoom range completo

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 
 import * as mapboxgl  from "mapbox-gl";
 
@@ -26,38 +26,56 @@ import * as mapboxgl  from "mapbox-gl";
     
   ]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 10;
+  center: [number, number] = [-99.58556475717162, 19.126198396960966];
 
   constructor() { }
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
+  }
 
   ngAfterViewInit(): void {
 
     console.log('AfterViewInit', this.divMapa);
     
-
+    // Mapa
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement, // container ID, que traemos desde el html con el viewchild
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [-99.58556475717162, 19.126198396960966], // starting position [lng, lat]
+      center: this.center, // starting position [lng, lat]
       zoom: this.zoomLevel, // starting zoom
     });
 
+    // Nivel del zoom
     this.mapa.on('zoom', (ev) => {
-      const zoomActual = this.mapa.getZoom();
-      // console.log(zoomActual);
-      this.zoomLevel = zoomActual;
+      this.zoomLevel = this.mapa.getZoom();
       // console.log(ev);
     });
 
+  // Nivel máximo del zoom
     this.mapa.on('zoomend', (ev) => {
       if ( this.mapa.getZoom() > 18 ){
         this.mapa.zoomTo( 18 );
       }
     });
+
+    // Movimiento del mapa
+    this.mapa.on('move', (event) => {
+      // console.log(event);
+      const target = event.target;
+      const {lng, lat} = target.getCenter();
+      // console.log(target.getCenter());
+      this.center = [lng , lat];
+    });
+
+
   }
   zoomOut(){
     // console.log('zoom out');
